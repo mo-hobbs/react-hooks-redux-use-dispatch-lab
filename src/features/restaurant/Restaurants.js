84e3fcaab5e9c165a878dea0242d4a7797b1f59d
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-04-15 09:25:46 -0400

    test pass

--- src/features/restaurant/Restaurants.js
+++ src/features/restaurant/Restaurants.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function Restaurants() {
   const restaurants = useSelector((state) => state.restaurants);
